62368ef02c0dab2130f0885b2ab5e346f224af72
Jhon Arias <157815560+Arias-Hen@users.noreply.github.com>
2025-11-24 10:52:03 -0400

    Update settings.py

--- ApiIgnite/settings.py
+++ ApiIgnite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
-ALLOWED_HOSTS = ['3.149.1.189', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app','3.149.1.189', 'localhost']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
